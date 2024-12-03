6014c2e8cb4b5e1420f4ba374f4b20aa6f728ec9
nakata <c0b2310742@edu.teu.ac.jp>
2024-12-03 14:29:50 +0900

    練習2　

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -153,9 +153,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            # if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-            #     # スペースキー押下でBeamクラスのインスタンス生成
-            #     beam = Beam(bird)            
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                # スペースキー押下でBeamクラスのインスタンス生成
+                beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
         if bird.rct.colliderect(bomb.rct):
@@ -165,11 +165,29 @@ def main():
             time.sleep(1)
             return
 
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
+        if beam is not None:
+            if beam.rct.colliderect(bomb.rct):  # ビームが爆弾を撃ち落としたら
+                beam = None
+                bomb = None
+
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         beam.update(screen)
         # beam.update(screen)   
         bomb.update(screen)
+         # beam.update(screen)
+        if bomb is not None:
+            bomb.update(screen)
+        if beam is not None:
+            beam.update(screen)
+        # bomb2.update(screen) 
         pg.display.update()
         tmr += 1
         clock.tick(50)
